265f86189bf23506d443aeef1c9a8c756c834785
brendanoc21 <brendanoconnor2030@gmail.com>
2024-10-01 13:49:49 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -10,11 +10,19 @@ function App() {
       { id: 3, title: "Tidy up", deadline: "Today", done: false}
     ]
   });
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
     setTaskState({tasks});
   }
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -26,6 +34,7 @@ function App() {
           key={task.id}
           done={task.done}
           markDone={() => doneHandler(index)}
+          deleteTask = {() => deleteHandler(index)}
         />
       ))}
     </div>
